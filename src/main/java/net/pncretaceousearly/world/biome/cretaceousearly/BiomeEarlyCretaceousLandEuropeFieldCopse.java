1e86f4c10ca223cd19cce02dbdc44c4e4ab26388
AechtRob <Aecht_Rob@outlook.com>
2024-05-04 09:40:45 +0100

    First version

--- src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousLandEuropeFieldCopse.java
+++ src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousLandEuropeFieldCopse.java
@@ -188,12 +188,21 @@ public class BiomeEarlyCretaceousLandEuropeFieldCopse extends ElementsLepidodend
 				}
 
 			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
-				for (int i = 0; i < 8; ++i)
+				for (int i = 0; i < 12; ++i)
 				{
 					int j = rand.nextInt(16) + 8;
 					int k = rand.nextInt(16) + 8;
 					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
 					PLANT_GENERATOR.generate(BlockDoratophyllum.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 255, true);
+				}
+
+			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
+				for (int i = 0; i < 3; ++i)
+				{
+					int j = rand.nextInt(16) + 8;
+					int k = rand.nextInt(16) + 8;
+					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
+					PLANT_GENERATOR.generate(BlockRoseAncient.block.getDefaultState(), worldIn, rand, pos.add(j, l, k));
 				}
 
 			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
