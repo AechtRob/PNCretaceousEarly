41210560c42be0853c9fc4f858f32e4b23cc4e18
AechtRob <Aecht_Rob@outlook.com>
2024-06-15 15:58:57 +0100

    First version

--- src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousCreekSouthAmericanArid.java
+++ src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousCreekSouthAmericanArid.java
@@ -100,6 +100,15 @@ public class BiomeEarlyCretaceousCreekSouthAmericanArid extends ElementsLepidode
 					int j = rand.nextInt(16) + 8;
 					int k = rand.nextInt(16) + 8;
 					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
+					PLANT_GENERATOR.generate(BlockWelwitschiophyllum.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 70, true);
+				}
+
+			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
+				for (int i = 0; i < 64; ++i)
+				{
+					int j = rand.nextInt(16) + 8;
+					int k = rand.nextInt(16) + 8;
+					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
 					PLANT_GENERATOR.generate(BlockAridHorsetail.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 70, true);
 				}
 
--- src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousLandSouthAmericanArid.java
+++ src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousLandSouthAmericanArid.java
@@ -105,13 +105,23 @@ public class BiomeEarlyCretaceousLandSouthAmericanArid extends ElementsLepidoden
 //					ROCK_GENERATOR.generate(worldIn, rand, worldIn.getTopSolidOrLiquidBlock(new BlockPos(pos.getX() + j, 0, pos.getZ() + k)).up());
 //				}
 //
+//
 			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
-				for (int i = 0; i < 64; ++i)
+				for (int i = 0; i < 24; ++i)
 				{
 					int j = rand.nextInt(16) + 8;
 					int k = rand.nextInt(16) + 8;
 					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
-					PLANT_GENERATOR.generate(BlockAridHorsetail.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 70, true);
+					PLANT_GENERATOR.generate(BlockWelwitschiophyllum.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 70, false);
+				}
+
+			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
+				for (int i = 0; i < 24; ++i)
+				{
+					int j = rand.nextInt(16) + 8;
+					int k = rand.nextInt(16) + 8;
+					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
+					PLANT_GENERATOR.generate(BlockAridHorsetail.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 70, false);
 				}
 //
 //			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
