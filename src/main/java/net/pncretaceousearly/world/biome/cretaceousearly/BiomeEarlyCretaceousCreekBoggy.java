d415cd3f710aacf53d0d29d6b00d9756e59d2763
AechtRob <Aecht_Rob@outlook.com>
2024-07-31 21:19:24 +0100

    First version

--- src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousCreekBoggy.java
+++ src/main/java/net/pncretaceousearly/world/biome/cretaceousearly/BiomeEarlyCretaceousCreekBoggy.java
@@ -43,7 +43,7 @@ public class BiomeEarlyCretaceousCreekBoggy extends ElementsLepidodendronMod.Mod
 	static class BiomeGenCustom extends BiomeCretaceousEarly {
 		public BiomeGenCustom() {
 			//was height 0.001
-			super(new BiomeProperties("E. Cretaceous Boggy Glades Creek").setBaseHeight(-0.50F).setHeightVariation(0.01F).setRainfall(1.5F));
+			super(new BiomeProperties("E. Cretaceous Boggy Glades Creek").setBaseHeight(-0.50F).setHeightVariation(0.01F).setRainfall(1.5F).setWaterColor(-7562693));
 			setRegistryName("lepidodendron:cretaceous_early_creek_boggy_glades");
 			topBlock = BlockPrehistoricGroundFern.block.getDefaultState();
 			fillerBlock = Blocks.DIRT.getStateFromMeta(2);
